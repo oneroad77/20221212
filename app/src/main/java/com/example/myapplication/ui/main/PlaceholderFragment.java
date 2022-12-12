17ae3d8b9dbc59e657f9d226b2004fcde6504f85
oneroad77 <promise200077@gmail.com>
2022-12-12 14:57:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/main/PlaceholderFragment.java
@@ -0,0 +1,85 @@
+package com.example.myapplication.ui.main;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentMainBinding;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+    private FragmentMainBinding binding;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        int position = getArguments().getInt(ARG_SECTION_NUMBER);
+    switch (position) {
+        case 1:
+            root.setBackgroundColor(Color.rgb(0xFF, 0xAB, 0X91));
+            break;
+        case 2:
+            root.setBackgroundColor(Color.rgb(0xFF, 0xCC, 0X80));
+            break;
+        case 3:
+            root.setBackgroundColor(Color.rgb(0xA5, 0xD6, 0XA7));
+            break;
+        case 4:
+            root.setBackgroundColor(Color.rgb(0x80, 0xDE, 0XEA));
+            break;
+}
+        final TextView textView = binding.sectionLabel;
+        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
